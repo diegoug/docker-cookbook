2917caadbb418772c3348e4a37a5fbef1473008d
Diego Uribe Gamez <diego.uribe.gamez@gmail.com>
2016-09-17 15:12:14 -0500

    [update] django virtual env database

--- django/app/mysite/settings.py
+++ django/app/mysite/settings.py
@@ -11,7 +11,7 @@ TEMPLATE_DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DATABASE_NAME', ''),
+        'NAME': os.environ.get('MYSQL_DATABASE_NAME', ''),
         'USER': os.environ.get('MYSQL_USER', ''),
         'PASSWORD': os.environ.get('MYSQL_PASSWORD', ''),
         'HOST': os.environ.get('MYSQL_HOST', ''),
