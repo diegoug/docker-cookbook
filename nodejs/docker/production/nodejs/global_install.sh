59ff4d21c11e544b3fb6389a6de174bbe908bb83
Diego Uribe Gamez <diego.uribe.gamez@gmail.com>
2016-11-07 20:19:36 -0500

    [remove] nodejs custom  config

--- nodejs/docker/production/nodejs/global_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-__install_requirements() {
-npm install -g node-inspector@0.9.2 supervisor@0.6.0 forever@0.14.1
-}
-
-__install_requirements
--- nodejs/docker/production/nodejs/npm_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-__install_requirements() {
-cd /opt/nodejs/
-npm install
-}
-
-__install_requirements
